c30c921154ffc7caa6726035d00ef933e1595bb7
ssaidy <salieu.saidy.2021@live.rhul.ac.uk>
2023-11-09 00:01:27 +0000

    Added Navigation component and side bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { AiOutlineMenu } from 'react-icons/ai'
+const Navbar = () => {
+    return (
+        <div>
+            <div>
+            <AiOutlineMenu size = {30} />
+            </div>
+            <h1>
+                E <span>Clo</span>
+            </h1>
+    </div>
+    )
+}
+
+export default Navbar
