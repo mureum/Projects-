33857b984744190cb3426625b4ee01065cdc3479
ssaidy <salieu.saidy.2021@live.rhul.ac.uk>
2023-11-16 22:04:07 +0000

    Added the optional removal of the navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, {useState}from 'react'
 import { AiOutlineMenu,AiOutlineSearch, AiOutlineClose } from 'react-icons/ai'
 import {BsFillCartFill} from 'react-icons/bs'
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
     return (
         <div className='max-w-[1640px] mx-auto flex justify-between items-center p-4'>
 
             <div className='flex items-center'>
-                <div>
+                <div onClick={() => setNav(!nav)}>
                 <AiOutlineMenu size = {30} />
                 </div>
                 <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>
@@ -24,12 +25,13 @@ const Navbar = () => {
         <button className='bg-black text-white md:flex items-center py-2'>
            <BsFillCartFill/ > Shopping Basket     
         </button>
-        <div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0 '></div>
+        
+        {nav ? <div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0 '></div> : '' }
        
 
        {/* Side Draw menu */}
-   <div className='fixed top-0 left-0 w-[50%] h-screen bg-white z-10 duration-300'>
-   <AiOutlineClose size={30} className='absolute right-4 top-4 cursor-pointer'/>
+       <div className={nav ? 'fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300' : 'fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300' }>    
+       <AiOutlineClose onClick={()=> setNav(!nav)} size={30} className='absolute right-4 top-4 cursor-pointer'/>
    <h2 className='text-2xl p-4'>
        E <span className='font-bold'>Clo</span>
    </h2>
