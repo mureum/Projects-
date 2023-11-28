0db4b0551abdaa9747074837cfb4f8e5372491d3
ssaidy <salieu.saidy.2021@live.rhul.ac.uk>
2023-11-28 15:23:12 +0000

    Added Bacground image to Main page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Main({ onButtonClick }) {
   return (
-    <div className="bg-cover bg-center min-h-screen flex flex-col items-center justify-center" >
+    <div className="bg-cover bg-center min-h-screen flex flex-col items-center justify-center" style={{backgroundImage: 'url("https://images.unsplash.com/photo-1545486332-9e0999c535b2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGRhcmslMjBiYWNrZ3JvdW5kfGVufDB8fDB8fHww")'}}>
       <h1 className="text-white text-4xl font-bold mb-4">Main</h1>
-      <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={onButtonClick}>
+      <button className="bg-blue-500 text-white px-8 py-2 rounded" onClick={onButtonClick}>
         Show Webpage
       </button>
     </div>
