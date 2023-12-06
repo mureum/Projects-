95d43a63f8d4189bc49e6d064a824a646e5e07cd
ssaidy <salieu.saidy.2021@live.rhul.ac.uk>
2023-12-06 01:23:41 +0000

    Create and place widgets for password button

--- GUI/gui.py
+++ GUI/gui.py
@@ -12,5 +12,13 @@ label_username.grid(row=0, column=0, padx=10, pady=10, sticky=tk.W)
 entry_username = tk.Entry(root)
 entry_username.grid(row=0, column=1, padx=10, pady=10)
 
+# Create Password button
+label_password = tk.Label(root, text="Password:")
+label_password.grid(row=1, column=0, padx=10, pady=10, sticky=tk.W)
+
+entry_password = tk.Entry(root, show="*")
+entry_password.grid(row=1, column=1, padx=10, pady=10)
+
+
 # Start the main event loop
 root.mainloop()
