0f8f580ab1a51e435413e3a21eee89a595f72958
ssaidy <salieu.saidy.2021@live.rhul.ac.uk>
2023-12-06 01:22:01 +0000

    Created widget for username button

--- GUI/gui.py
+++ GUI/gui.py
@@ -5,5 +5,12 @@ from tkinter import messagebox
 root = tk.Tk()
 root.title("Login System")
 
+# Create username button
+label_username = tk.Label(root, text="Username:")
+label_username.grid(row=0, column=0, padx=10, pady=10, sticky=tk.W)
+
+entry_username = tk.Entry(root)
+entry_username.grid(row=0, column=1, padx=10, pady=10)
+
 # Start the main event loop
 root.mainloop()
