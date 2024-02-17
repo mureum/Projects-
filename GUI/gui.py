fd65f4059f4f61c0e483c751ee0bde5136777a82
ssaidy <salieu.saidy.2021@live.rhul.ac.uk>
2024-02-17 17:55:28 +0000

    Created a database connection and added a table for user entries

--- GUI/gui.py
+++ GUI/gui.py
@@ -2,6 +2,23 @@ import tkinter as tk
 from tkinter import messagebox
 import sqlite3
 
+# Created a database connection to store user data in the "loginpage" database 
+
+def connect():
+    conn = sqlite3.connect("loginpage.db")
+    cur = conn.cursor()
+
+    # Creates the table and commits to changes to the database
+    cur.execute("CREATE TABLE IF NOT EXISTS users(name TEXT,username TEXT,password TEXT)")
+    conn.commit()
+
+    # Closes the database connection -> It's good practise to close connectiuon when no longer needed
+
+    conn.close()
+
+# Ensures database connection is established
+connect()
+
 def validate_login():
     username = entry_username.get()
     password = entry_password.get()
