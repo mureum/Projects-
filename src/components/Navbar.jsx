76221086ba0a831334d0c7d1954833b2535b5094
ssaidy <salieu.saidy.2021@live.rhul.ac.uk>
2024-03-30 15:21:15 +0000

    Implement Navbar component.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,220 @@
-import React from 'react'
-import { AiOutlineMenu } from 'react-icons/ai'
-const Navbar = () => {
-    return (
-        <div>
-            <div>
-            <AiOutlineMenu size = {30} />
-            </div>
-            <h1>
-                E <span>Clo</span>
-            </h1>
-    </div>
-    )
-}
+import React, { useState } from "react";
+import { Link } from "react-scroll";
+import { GiAbstract044 } from "react-icons/gi";
+import Button from "../layouts/Button";
+import { AiOutlineMenuUnfold } from "react-icons/ai";
+import { BiChevronDown } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
 
-export default Navbar
+const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleChange = () => {
+    setMenu(!menu);
+  };
+
+  const closeMenu = () => {
+    setMenu(false);
+  };
+
+  const toggleTheme = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <>
+      <div className={`fixed w-full ${darkMode ? 'dark' : ''}`}>
+        <div>
+          <div className="flex flex-row justify-between p-5 md:px-32 px-5 bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+            <div className="flex flex-row items-center cursor-pointer">
+              <span>
+                <GiAbstract044 size={32} />
+              </span>
+              <h1 className="text-xl font-semibold">Culture Shock</h1>
+            </div>
+
+            <nav className="hidden md:flex flex-row items-center text-lg font-medium gap-8">
+              <Link
+                to="home"
+                spy={true}
+                smooth={true}
+                duration={500}
+                className={`hover:text-${darkMode ? 'gray-300' : 'brightColor'} transition-all cursor-pointer`}
+              >
+                Home
+              </Link>
+
+              <div className="relative group">
+                <div className="flex items-center gap-1">
+                  <Link
+                    to="dishes"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    className="hover:text-brightColor transition-all cursor-pointer"
+                  >
+                    Clothes
+                  </Link>
+
+                  <BiChevronDown className="cursor-pointer" size={25} />
+                </div>
+
+                <ul className="absolute hidden space-y-2 group-hover:block bg-white border border-gray-300 rounded-lg p-5">
+                  <li>
+                    <Link
+                      to="dishes"
+                      spy={true}
+                      smooth={true}
+                      duration={500}
+                      className="text-gray-800 hover:text-brightColor transition-all cursor-pointer"
+                    >
+                      Streetwear
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="dishes"
+                      spy={true}
+                      smooth={true}
+                      duration={500}
+                      className="text-gray-800 hover:text-brightColor transition-all cursor-pointer"
+                    >
+                      Casual
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="dishes"
+                      spy={true}
+                      smooth={true}
+                      duration={500}
+                      className="text-gray-800 hover:text-brightColor transition-all cursor-pointer"
+                    >
+                      Formal
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="dishes"
+                      spy={true}
+                      smooth={true}
+                      duration={500}
+                      className="text-gray-800 hover:text-brightColor transition-all cursor-pointer"
+                    >
+                      Fashionista
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+
+              <Link
+                to="about"
+                spy={true}
+                smooth={true}
+                duration={500}
+                className="hover:text-brightColor transition-all cursor-pointer"
+              >
+                About
+              </Link>
+
+              <Link
+                to="menu"
+                spy={true}
+                smooth={true}
+                duration={500}
+                className="hover:text-brightColor transition-all cursor-pointer"
+              >
+                Premium
+              </Link>
+
+              <Link
+                to="review"
+                spy={true}
+                smooth={true}
+                duration={500}
+                className="hover:text-brightColor transition-all cursor-pointer"
+              >
+                Reviews
+              </Link>
+
+              <Button title="Login" />
+            </nav>
+
+            <div className="md:hidden flex items-center">
+              {menu ? (
+                <AiOutlineClose size={25} onClick={handleChange} />
+              ) : (
+                <AiOutlineMenuUnfold size={25} onClick={handleChange} />
+              )}
+            </div>
+          </div>
+          <div className={`${menu ? "translate-x-0" : "-translate-x-full"} lg:hidden flex flex-col absolute bg-black text-white left-0 top-20 font-semibold text-2xl text-center pt-8 pb-4 gap-8 w-full h-fit transition-transform duration-300`}>
+            <Link
+              to="home"
+              spy={true}
+              smooth={true}
+              duration={500}
+              className="hover:text-brightColor transition-all cursor-pointer"
+              onClick={closeMenu}
+            >
+              Home
+            </Link>
+            <Link
+              to="dishes"
+              spy={true}
+              smooth={true}
+              duration={500}
+              className="hover:text-brightColor transition-all cursor-pointer"
+              onClick={closeMenu}
+            >
+              Clothes
+            </Link>
+            <Link
+              to="about"
+              spy={true}
+              smooth={true}
+              duration={500}
+              className="hover:text-brightColor transition-all cursor-pointer"
+              onClick={closeMenu}
+            >
+              About
+            </Link>
+            <Link
+              to="menu"
+              spy={true}
+              smooth={true}
+              duration={500}
+              className="hover:text-brightColor transition-all cursor-pointer"
+              onClick={closeMenu}
+            >
+              Catalogue
+            </Link>
+            <Link
+              to="review"
+              spy={true}
+              smooth={true}
+              duration={500}
+              className="hover:text-brightColor transition-all cursor-pointer"
+              onClick={closeMenu}
+            >
+              Reviews
+            </Link>
+
+            <Button title="login" />
+          </div>
+        </div>
+      </div>
+      <div className="fixed bottom-5 right-5">
+        <button
+          className={`p-2 rounded-full ${darkMode ? 'bg-gray-800 text-white' : 'bg-white text-black'} shadow-md`}
+          onClick={toggleTheme}
+        >
+          {darkMode ? 'Light Mode' : 'Dark Mode'}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
