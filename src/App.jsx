40c4ab1fdcc024a619664423adb2fabc9fbc7ca7
ssaidy <salieu.saidy.2021@live.rhul.ac.uk>
2024-03-30 15:46:56 +0000

    Fixed errors in the pakage-lock.json file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <div id="home">
+          <Home />
+        </div>
 
     </div>
   );
